2c71974928f56dc032df3f48a218059fb6435512
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-07 06:30:58 -0700

    parse json

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //next signals the end of the middleware function. Express relies on teh next function to know when to move forward, ie when a middleware function is done. If it isn't given, it will hang indefinitely. You must end middleware by either calling next or sending a response.
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');

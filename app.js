5de4a260d45dd567259f07546dfa011f1c7ff406
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-07 07:54:27 -0700

    set up gh-pages

--- app.js
+++ app.js
@@ -31,3 +31,20 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
 	console.log('The application is running on localhost:3000!')
 });
+
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', function(err) {
+
+});
+
+
+// const http = require('http');
+// const port = process.env.PORT || 3000;
+//
+// http.createServer(function(req, res){
+// 	res.writeHead(200, {'Content-Type': 'text/plain'});
+// 	res.end('Hello Heroku\n');
+// }).listen(port, function(){
+// 	console.log("Server running at http://" + port + "/");
+// });

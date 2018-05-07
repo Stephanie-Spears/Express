bb496cc227cd8dc954724d735d20c48f17e8050f
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-06 20:52:46 -0700

    update port

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const http = require('http');
-const hostname = '127.0.0.1';
 const port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});
 	response.end('Hello Heroku\n');
-}).listen(port, hostname, function(){
-	console.log("Server running at http://" + hostname + ":" + port + "/");
+}).listen(port, function(){
+	console.log("Server running at http://" + port + "/");
 });
 
 // const express = require('express');

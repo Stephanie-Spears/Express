5af9ca58e5a7524b62aebb4feb3e9ec42c16a95e
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-07 06:11:46 -0700

    add callback arguement "next" to the middleware function, which parses the response object

--- app.js
+++ app.js
@@ -9,20 +9,6 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-	console.log("Hello");
-	const err = new Error('Oh noes!');
-	next(err);
-});
-
-app.use((req, res, next) => {
-	console.log("world");
-	next();
-});
-
-
-
-
 app.get('/', (req, res) => {
 	const name = req.cookies.username;
 	if (name) {
@@ -53,6 +39,18 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
 	res.clearCookie('username');
 	res.redirect('/hello');
+});
+
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(err.status);
+	res.render('error');
 });
 
 app.listen(3000, () => {

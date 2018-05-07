82014380530f72bd72dea7909b48d07e80af0060
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-06 20:51:15 -0700

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 1337;
+const port = process.env.PORT || 1337;
 
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/plain'});

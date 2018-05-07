a40fb354d05e5488468e5e885468163a7f9819f7
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-06 22:21:04 -0700

    separate pug template partials

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
 const express = require('express');
 
-const app = express(); //returns an express application
-
+const app = express();
 
 app.set('view engine', 'pug');
 
-//A route is where a server is set up to respond to a request from a client.
 app.get('/', (req, res) => {
 	res.render('index');
 });
 
-
-
 app.get('/cards', (req, res) => {
-	res.render('card', {prompt: "Who is burried in Grant's tomb?"});
+	res.render('card', { prompt: "Who is buried in Grant's tomb?" });
 });
 
 
-
-
 app.listen(3000, () => {
-	console.log("The application is running.");
-}); //
-
-
-
-
-
-
-
-
-
-//
-// const http = require('http');
-// const port = process.env.PORT || 1337;
-//
-// http.createServer(function(request, response){
-// 	response.writeHead(200, {'Content-Type': 'text/plain'});
-// 	response.end('Hello Heroku\n');
-// }).listen(port, function(){
-// 	console.log("Server running at http://" + port + "/");
-// });
+	console.log('The application is running on localhost:3000!')
+});
